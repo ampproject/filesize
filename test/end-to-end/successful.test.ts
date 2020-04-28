16de5d63564be2590dfbbb2c0aacecf09e6b1ef5
Kristofer Baxter <kristofer@kristoferbaxter.com>
2020-04-28 09:20:13 -0700

    Add -c config option for users preferring to avoid package.json (#87)
    * Add -c config option for users preferring to avoid package.json
    * Pushed Changes from Github Actions Bot
    * Support config in API, add success and failure tests
    * Pushed Changes from Github Actions Bot
    * Update readme

--- test/end-to-end/successful.test.ts
+++ test/end-to-end/successful.test.ts
@@ -20,10 +20,17 @@ import { report, Report } from '../../src/api';
 import { SizeMapValue, SizeMap } from '../../src/validation/Condition';
 import { resolve, relative } from 'path';
 
-const toReport = 'test/end-to-end/fixtures/successful';
-
 test.cb('item under requested filesize limit passes', (t) => {
   const executeSuccess = exec('./dist/filesize -p=test/end-to-end/fixtures/successful');
+
+  executeSuccess.on('exit', (code) => {
+    t.is(code, 0);
+    t.end();
+  });
+});
+
+test.cb('standalone configuration file when valid should pass', (t) => {
+  const executeSuccess = exec('./dist/filesize -c=test/end-to-end/fixtures/successful/filesize.json');
 
   executeSuccess.on('exit', (code) => {
     t.is(code, 0);
@@ -40,7 +47,20 @@ test('item under requested filesize limit passes from API', async (t) => {
   const expected: SizeMap = new Map();
   expected.set(resolve('test/end-to-end/fixtures/successful/index.js'), sizes);
 
-  const results = await report(toReport, null);
+  const results = await report('test/end-to-end/fixtures/successful', null);
+  t.deepEqual(results, expected);
+});
+
+test('item under requested filesize limit passes from API, using configuration file', async (t) => {
+  const sizes: SizeMapValue = [
+    [3410, 3584], // brotli
+    [3737, 4096], // gzip
+    [9327, 10240], // none
+  ];
+  const expected: SizeMap = new Map();
+  expected.set(resolve('test/end-to-end/fixtures/successful/index.js'), sizes);
+
+  const results = await report('test/end-to-end/fixtures/successful/filesize.json', null);
   t.deepEqual(results, expected);
 });
 
@@ -53,12 +73,28 @@ test('item under requested filesize limit passes from API, with replacement', as
   const expected: SizeMap = new Map();
   expected.set(resolve('test/end-to-end/fixtures/successful/index.js'), sizes);
 
-  const results = await report(toReport, (content) => content.replace(new RegExp('preact.umd.js.map', 'g'), 'FOO.map'));
+  const results = await report('test/end-to-end/fixtures/successful', (content) =>
+    content.replace(new RegExp('preact.umd.js.map', 'g'), 'FOO.map'),
+  );
+  t.deepEqual(results, expected);
+});
+
+test('item under requested filesize limit passes from API, using configuration file, with replacement', async (t) => {
+  const sizes: SizeMapValue = [
+    [3401, 3584], // brotli
+    [3731, 4096], // gzip
+    [9317, 10240], // none
+  ];
+  const expected: SizeMap = new Map();
+  expected.set(resolve('test/end-to-end/fixtures/successful/index.js'), sizes);
+
+  const results = await report('test/end-to-end/fixtures/successful/filesize.json', (content) =>
+    content.replace(new RegExp('preact.umd.js.map', 'g'), 'FOO.map'),
+  );
   t.deepEqual(results, expected);
 });
 
 test('api is interactive with custom reporter', async (t) => {
-  const toReport = 'test/end-to-end/fixtures/api-report';
   const mapping = new Map([
     ['preact.js', 3477],
     ['inferno.js', 7297],
@@ -73,7 +109,31 @@ test('api is interactive with custom reporter', async (t) => {
         const completed = super.getUpdated(context);
         for (const complete of completed) {
           const [filePath, sizeMap] = complete;
-          const relativePath = relative(toReport, filePath);
+          const relativePath = relative('test/end-to-end/fixtures/api-report', filePath);
+          t.is(mapping.has(relativePath), true);
+          t.is(mapping.get(relativePath), sizeMap[0][0]);
+        }
+      }
+    },
+  );
+});
+
+test('api is interactive with custom reporter, using configuration file', async (t) => {
+  const mapping = new Map([
+    ['preact.js', 3477],
+    ['inferno.js', 7297],
+    ['react-dom.js', 28721],
+  ]);
+
+  await report(
+    'test/end-to-end/fixtures/api-report/filesize.json',
+    (content) => content,
+    class extends Report {
+      update(context: any) {
+        const completed = super.getUpdated(context);
+        for (const complete of completed) {
+          const [filePath, sizeMap] = complete;
+          const relativePath = relative('test/end-to-end/fixtures/api-report', filePath);
           t.is(mapping.has(relativePath), true);
           t.is(mapping.get(relativePath), sizeMap[0][0]);
         }
